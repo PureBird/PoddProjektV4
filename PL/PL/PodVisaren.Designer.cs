f9f055d831d218a6c91837193ec0a286f27a6fe3
eriksundenius <erik.sundenius@gmail.com>
2025-11-18 17:44:59 +0100

    visar avsnitt

--- PL/PL/PodVisaren.Designer.cs
+++ PL/PL/PodVisaren.Designer.cs
@@ -32,61 +32,85 @@
             RSSTEXT = new TextBox();
             visaPODD = new Button();
             poddTabell = new DataGridView();
+            avsnittGrid = new DataGridView();
             ((System.ComponentModel.ISupportInitialize)poddTabell).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)avsnittGrid).BeginInit();
             SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
-            label1.BackColor = SystemColors.ControlLightLight;
-            label1.Font = new Font("Stencil", 26F, FontStyle.Bold | FontStyle.Italic, GraphicsUnit.Point, 0);
-            label1.Location = new Point(2, 9);
+            label1.BackColor = SystemColors.ActiveCaption;
+            label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label1.Location = new Point(11, 9);
+            label1.Margin = new Padding(2, 0, 2, 0);
             label1.Name = "label1";
-            label1.Size = new Size(786, 61);
+            label1.Size = new Size(540, 47);
             label1.TabIndex = 2;
             label1.Text = "SÖK EFTER DIN FAVORITPODD";
-            //label1.Click += label1_Click;
             // 
             // RSSTEXT
             // 
-            RSSTEXT.Location = new Point(98, 103);
+            RSSTEXT.BackColor = SystemColors.HighlightText;
+            RSSTEXT.Location = new Point(11, 62);
+            RSSTEXT.Margin = new Padding(2);
             RSSTEXT.Name = "RSSTEXT";
-            RSSTEXT.Size = new Size(332, 31);
+            RSSTEXT.PlaceholderText = "Länka din podd i rss format!";
+            RSSTEXT.Size = new Size(303, 23);
             RSSTEXT.TabIndex = 3;
             RSSTEXT.TextChanged += RSSTEXT_TextChanged;
             // 
             // visaPODD
             // 
-            visaPODD.Location = new Point(622, 104);
+            visaPODD.Location = new Point(318, 62);
+            visaPODD.Margin = new Padding(2);
             visaPODD.Name = "visaPODD";
-            visaPODD.Size = new Size(112, 34);
+            visaPODD.Size = new Size(88, 23);
             visaPODD.TabIndex = 4;
-            visaPODD.Text = "button1";
+            visaPODD.Text = "Visa din podd";
             visaPODD.UseVisualStyleBackColor = true;
             visaPODD.Click += visaPODD_Click_1;
             // 
             // poddTabell
             // 
+            poddTabell.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            poddTabell.BackgroundColor = SystemColors.ButtonHighlight;
             poddTabell.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            poddTabell.Location = new Point(12, 183);
+            poddTabell.Location = new Point(11, 117);
+            poddTabell.Margin = new Padding(2);
             poddTabell.Name = "poddTabell";
             poddTabell.RowHeadersWidth = 62;
-            poddTabell.Size = new Size(722, 225);
+            poddTabell.Size = new Size(432, 69);
             poddTabell.TabIndex = 5;
             poddTabell.CellContentClick += poddTabell_CellContentClick;
             // 
+            // avsnittGrid
+            // 
+            avsnittGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+            avsnittGrid.BackgroundColor = SystemColors.ButtonHighlight;
+            avsnittGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            avsnittGrid.Location = new Point(448, 117);
+            avsnittGrid.Name = "avsnittGrid";
+            avsnittGrid.Size = new Size(624, 370);
+            avsnittGrid.TabIndex = 6;
+            avsnittGrid.CellContentClick += avsnittGrid_CellContentClick;
+            // 
             // PodVisaren
             // 
-            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
+            BackColor = SystemColors.ActiveCaption;
+            ClientSize = new Size(1084, 515);
+            Controls.Add(avsnittGrid);
             Controls.Add(poddTabell);
             Controls.Add(visaPODD);
             Controls.Add(RSSTEXT);
             Controls.Add(label1);
+            Margin = new Padding(2);
             Name = "PodVisaren";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)poddTabell).EndInit();
+            ((System.ComponentModel.ISupportInitialize)avsnittGrid).EndInit();
             ResumeLayout(false);
             PerformLayout();
         }
@@ -96,5 +120,6 @@
         private TextBox RSSTEXT;
         private Button visaPODD;
         private DataGridView poddTabell;
+        private DataGridView avsnittGrid;
     }
 }
