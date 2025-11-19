c258fdde2badb0ed03b81580b766b90bf69fccad
eriksundenius <erik.sundenius@gmail.com>
2025-11-19 11:16:21 +0100

    random

--- PL/PL/PodVisaren.Designer.cs
+++ PL/PL/PodVisaren.Designer.cs
@@ -42,30 +42,27 @@
             label1.AutoSize = true;
             label1.BackColor = SystemColors.ActiveCaption;
             label1.Font = new Font("Segoe UI", 26.25F, FontStyle.Bold, GraphicsUnit.Point, 0);
-            label1.Location = new Point(11, 9);
-            label1.Margin = new Padding(2, 0, 2, 0);
+            label1.Location = new Point(16, 15);
             label1.Name = "label1";
-            label1.Size = new Size(540, 47);
+            label1.Size = new Size(796, 71);
             label1.TabIndex = 2;
             label1.Text = "SÖK EFTER DIN FAVORITPODD";
             // 
             // RSSTEXT
             // 
             RSSTEXT.BackColor = SystemColors.HighlightText;
-            RSSTEXT.Location = new Point(11, 62);
-            RSSTEXT.Margin = new Padding(2);
+            RSSTEXT.Location = new Point(16, 103);
             RSSTEXT.Name = "RSSTEXT";
             RSSTEXT.PlaceholderText = "Länka din podd i rss format!";
-            RSSTEXT.Size = new Size(303, 23);
+            RSSTEXT.Size = new Size(431, 31);
             RSSTEXT.TabIndex = 3;
             RSSTEXT.TextChanged += RSSTEXT_TextChanged;
             // 
             // visaPODD
             // 
-            visaPODD.Location = new Point(318, 62);
-            visaPODD.Margin = new Padding(2);
+            visaPODD.Location = new Point(454, 103);
             visaPODD.Name = "visaPODD";
-            visaPODD.Size = new Size(88, 23);
+            visaPODD.Size = new Size(179, 38);
             visaPODD.TabIndex = 4;
             visaPODD.Text = "Visa din podd";
             visaPODD.UseVisualStyleBackColor = true;
@@ -76,11 +73,10 @@
             poddTabell.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             poddTabell.BackgroundColor = SystemColors.ButtonHighlight;
             poddTabell.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            poddTabell.Location = new Point(11, 117);
-            poddTabell.Margin = new Padding(2);
+            poddTabell.Location = new Point(16, 195);
             poddTabell.Name = "poddTabell";
             poddTabell.RowHeadersWidth = 62;
-            poddTabell.Size = new Size(432, 69);
+            poddTabell.Size = new Size(617, 115);
             poddTabell.TabIndex = 5;
             poddTabell.CellContentClick += poddTabell_CellContentClick;
             // 
@@ -89,24 +85,25 @@
             avsnittGrid.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
             avsnittGrid.BackgroundColor = SystemColors.ButtonHighlight;
             avsnittGrid.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            avsnittGrid.Location = new Point(448, 117);
+            avsnittGrid.Location = new Point(640, 195);
+            avsnittGrid.Margin = new Padding(4, 5, 4, 5);
             avsnittGrid.Name = "avsnittGrid";
-            avsnittGrid.Size = new Size(624, 370);
+            avsnittGrid.RowHeadersWidth = 62;
+            avsnittGrid.Size = new Size(891, 617);
             avsnittGrid.TabIndex = 6;
             avsnittGrid.CellContentClick += avsnittGrid_CellContentClick;
             // 
             // PodVisaren
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(10F, 25F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ActiveCaption;
-            ClientSize = new Size(1084, 515);
+            ClientSize = new Size(1549, 858);
             Controls.Add(avsnittGrid);
             Controls.Add(poddTabell);
             Controls.Add(visaPODD);
             Controls.Add(RSSTEXT);
             Controls.Add(label1);
-            Margin = new Padding(2);
             Name = "PodVisaren";
             Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)poddTabell).EndInit();
