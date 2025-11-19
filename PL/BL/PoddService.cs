38cd74b0758983a3e284fd9f65e9e5a16d064a9d
FridaSel <frida.selin@gmail.com>
2025-11-19 10:48:57 +0100

    References updatering

--- PL/BL/PoddService.cs
+++ PL/BL/PoddService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using PoddProjektV4.DAL;
+using PoddProjektV4.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,20 @@ namespace PoddProjektV4.BL
 {
     internal class PoddService
     {
+
+        private readonly PoddRepository repository;
+        public PoddService(PoddRepository repository)
+        {
+            this.repository = repository;
+        }
+        public async Task<bool> TaBortEnKategoriFranPoddAsync(string poddId, string kategoriAttTaBort)
+        {
+            return await repository.TaBortEnKategoriFranPoddAsync(poddId, kategoriAttTaBort);
+        }
+        public async Task<int> TaBortKategoriFranAllaAsync(string kategori)
+        {
+            return await repository.TaBortKategoriFranAllaAsync(kategori);
+        }
+
     }
 }
